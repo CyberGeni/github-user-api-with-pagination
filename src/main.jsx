978d71e03dddd5a95fd7fd00fdb813e2f3626a18
Treasure Ajefu <treasureajefu@gmail.com>
2022-11-05 16:30:14 +0000

    setup page for error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
+import { BrowserRouter } from "react-router-dom";
+import Error from "./pages/Error";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
   <React.StrictMode>
-    
-    <App />
+    <BrowserRouter>
+      <ErrorBoundary FallbackComponent={Error}>
+        <App />
+      </ErrorBoundary>
+    </BrowserRouter>
   </React.StrictMode>
 )
