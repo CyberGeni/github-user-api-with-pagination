24b1ca0b7a691a391404bdf6a2cf7277293cea86
Treasure Ajefu <treasureajefu@gmail.com>
2022-11-05 21:12:11 +0000

    implemented repo info popup

--- src/pages/Repositories.jsx
+++ src/pages/Repositories.jsx
@@ -5,7 +5,7 @@ import { Link, Outlet } from "react-router-dom";
 
 function Repositories() {
   const [page, setPage] = useState(1);
-  let url = `https://api.github.com/users/cybergeni/repos?page=${page}&per_page=12&results=120`;
+  let url = `https://api.github.com/users/cybergeni/repos?page=${page}&per_page=9&results=120`;
   const { state } = useFetch(url);
   const { loading, user } = state;
   let pages = 8;
@@ -17,7 +17,7 @@ function Repositories() {
                 <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
                     {user &&
                     user.map((doc) => (
-                        <div key={doc.id} className="bg-[#1f2a48] rounded-md p-4">
+                        <div key={doc.id} className="bg-[#1f2a48] shadow-md rounded-md p-4">
                             <Link
                                 to={`cybergeni/${doc.name}`}
                                 state={{ doc: doc }}
@@ -35,18 +35,18 @@ function Repositories() {
                                 </div>
                                 <div className="mt-3">
                                     <h1 className="text-sm font-semibold text-slate-500">REPOSITORY NAME</h1>  
-                                    <p className="text-md font-semibold text-slate-50">{doc.name}</p> 
+                                    <p className="text-md font-semibold text-slate-50">{doc.name}</p>
                                 </div>
                             </Link>
                         </div>
                     ))}
                 </section>
                 <div className="">
-                    <div className="border border-slate-700 rounded-lg w-4/5 sm:w-fit mx-auto my-12">
+                    <div className="border border-slate-700 rounded-lg w-fit mx-auto my-12">
                         <button
                             disabled={page <= 1}
                             onClick={() => setPage((prev) => prev - 1)}
-                            className="px-6 py-4 active:bg-[#1f2a48] focus:bg-[#1f2a48] disabled:bg-gray-800 disabled:text-gray-600"
+                            className="p-2 sm:px-6 sm:py-4 active:bg-[#1f2a48] focus:bg-[#1f2a48] disabled:bg-gray-800 disabled:text-gray-600"
                         >
                             prev
                         </button>
@@ -55,7 +55,7 @@ function Repositories() {
                             <button
                                 key={btn}
                                 onClick={() => setPage(btn)}
-                                className="px-6 py-4 active:bg-[#1f2a48] focus:bg-[#1f2a48] rounded"
+                                className="px-2 sm:px-6 sm:py-4 active:bg-[#1f2a48] focus:bg-[#1f2a48] rounded"
                             >
                                 {btn}
                             </button>
@@ -64,7 +64,7 @@ function Repositories() {
                         <button
                             disabled={page >= 8}
                             onClick={() => setPage((prev) => prev + 1)}
-                            className="px-6 py-4 active:bg-[#1f2a48] focus:bg-[#1f2a48] hover:bg-[#1f2a48] disabled:bg-gray-800 disabled:text-gray-600"
+                            className="p-2 sm:px-6 sm:py-4 active:bg-[#1f2a48] focus:bg-[#1f2a48] hover:bg-[#1f2a48] disabled:bg-gray-800 disabled:text-gray-600"
                         >
                             next
                         </button>
--- src/pages/RepositoryInfo.jsx
+++ src/pages/RepositoryInfo.jsx
@@ -8,123 +8,58 @@ function RepositoryInfo() {
   let lastUpdate = new Date(doc.updated_at).toDateString()
 
   return (
-    <div>
-      <main>
-        
+<div className="relative flex items-center justify-center h-screen top-[-100vh] bg-[#141c2f] overflow-hidden">
+      <main className="absolute z-10 top-0 bg-[#1f2a48] grid grid-cols-2 shadow-md rounded-md my-auto p-8 mt-12 w-3/5">
+        <div>
+          <h1 className="text-sm font-semibold text-slate-500 mb-1">AUTHOR</h1>
+          <div className="flex items-center">
+            <img
+              src={doc.owner.avatar_url}
+              alt={doc.login}
+              className="w-8 h-8 mr-2 rounded-full"
+            />
+            <p className="text-slate-200">{doc.owner.login}</p>
+          </div>
+        </div>
+        <div className="mt-3">
+          <h1 className="text-sm font-semibold text-slate-500">
+            REPOSITORY NAME
+          </h1>
+          <a href={doc.svn_url} target="_blank">
+               <p className="text-md font-semibold text-slate-50">{doc.name}</p>
+          </a>
+        </div>
+        <div className="mt-3">
+          <h1 className="text-sm font-semibold text-slate-500">VISIBILITY</h1>
+          <p className="text-slate-200 capitalize">{doc.visibility}</p>
+        </div>
+        <div className="mt-3">
+          <h1 className="text-sm font-semibold text-slate-500">STARS</h1>
+          <p className="text-slate-200 capitalize">{doc.stargazers_count}</p>
+        </div>
+        <div className="mt-3">
+          <h1 className="text-sm font-semibold text-slate-500">FORKS</h1>
+          <p className="text-slate-200 capitalize">{doc.forks}</p>
+        </div>
+        <div className="mt-3">
+          <h1 className="text-sm font-semibold text-slate-500">LAST UPDATED</h1>
+          <p className="text-slate-200 capitalize">{lastUpdate}</p>
+        </div>
+        <div className="mt-3">
+          <h1 className="text-sm font-semibold text-slate-500">STACK</h1>
+          <p className="text-slate-200 capitalize ">{doc.language}</p>
+        </div>
+        <div className="mt-3 space-y-1">
+          <h1 className="text-sm font-semibold text-slate-500">REPOSITORY LINK</h1>
+          <a className="border border-slate-600 px-2 py-1 rounded-lg" href={doc.html_url} rel="noreferrer" target="_blank">View on Github</a>
+        </div>
       </main>
     </div>
-      // <div className="repo_de">
-      //   <div className="flex space-btw">
-      //     <div className="flex no-space">
-      //       <div>
-      //         <p>{doc.owner.login}</p>
-      //         <a href={doc.svn_url} target="_blank">
-      //           {doc.name}
-      //         </a>
-      //       </div>
 
-      //       <p className="public">{doc.visibility}</p>
-      //     </div>
-
-      //     {doc.stargazers_count > 0 && (
-      //       <div className="flex gap  no-space">
-      //         <img
-      //           src="/assets/icons8-golden-star.png"
-      //           className="star"
-      //           alt="golden star icon"
-      //         />
-      //         <p>Starred</p>
-      //       </div>
-      //     )}
-      //     {doc.stargazers_count === 0 && (
-      //       <div className="flex gap">
-      //         <img
-      //           src="/assets/icons8-bg-star.png"
-      //           className="star"
-      //           alt="star icon"
-      //         />
-      //         <p>Star</p>
-      //       </div>
-      //     )}
-      //   </div>
-      //   <div className="flex">
-      //     <div className="flex gap">
-      //       <img src="/assets/icons8-bg-star.png" className="star" />{" "}
-      //       <span> {doc.stargazers_count} star</span>
-      //     </div>
-
-      //     <div className="flex gap">
-      //       <img
-      //         src="/assets/icons8-forked.png"
-      //         className="star"
-      //         alt="fork icon"
-      //       />
-      //       <p>{doc.forks} fork</p>
-      //     </div>
-      //     <p>{doc.open_issues} issues</p>
-      //      <p>
-      //       Updated on {lastUpdate}
-      //     </p> 
-      //   </div>
-
-      //   {doc.description && <p>{doc.description}</p>}
-      //   {doc.language === "CSS" && (
-      //     <>
-      //       <p className="flex">
-      //         {" "}
-      //         <span
-      //           style={{
-      //             backgroundColor: "purple",
-      //             width: "15px",
-      //             borderRadius: "50%",
-      //             height: "15px",
-      //             display: "inline-block",
-      //           }}
-      //         ></span>
-      //         {doc.language}
-      //       </p>
-      //     </>
-      //   )}
-      //   {doc.language === "Javascript" && (
-      //     <>
-      //       <p className="flex">
-      //         {" "}
-      //         <span
-      //           style={{
-      //             backgroundColor: "yellow",
-      //             width: "15px",
-      //             borderRadius: "50%",
-      //             height: "15px",
-      //             display: "inline-block",
-      //           }}
-      //         ></span>
-      //         {doc.language}
-      //       </p>
-      //     </>
-      //   )}
-      //   {doc.language === "HTML" && (
-      //     <>
-      //       <p className="flex">
-      //         {" "}
-      //         <span
-      //           style={{
-      //             backgroundColor: "red",
-      //             width: "15px",
-      //             borderRadius: "50%",
-      //             height: "15px",
-      //             display: "inline-block",
-      //           }}
-      //         ></span>
-      //         {doc.language}
-      //       </p>
-      //     </>
-      //   )}
-
-      //   <a href={doc.html_url} rel="noreferrer" target="_blank">
-      //     view repository on githbub
-      //   </a>
-      // </div>
-
+    //   
+    //     view repository on githbub
+    //   </a>
+    // </div>
   );
 }
 export default RepositoryInfo;
